5e8bb4ef5b0119826eafcea83ee2333840d46e62
Keago99 <keatabde@gmail.com>
2023-09-06 20:00:55 +0200

    Create CSS style page, create basic table with borders.

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// I'll be overcommenting everything for learning purposes, apologies if this seems annoying
+
+
+// This is where all library files are stored
+const myLibrary = [];
+
+// Constructor for book object
+function book(author, title, pages, read){
+
+    this.author = author
+    this.title = title
+    this.pages = pages
+    this.read = read
+}
+
+function addBookToLibrary(){
+
+}
