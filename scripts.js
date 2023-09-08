5d9e942f9e5a2540df902c3b26985c4350d999a6
Keago99 <keatabde@gmail.com>
2023-09-08 18:13:36 +0200

    Complete form for addition of books to Arraylist added single book object to Arraylist.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
 // I'll be overcommenting everything for learning purposes, apologies if this seems annoying
 
+const signupForm = document.querySelector('[name="signup"]');
+
 
 // This is where all library files are stored
-const myLibrary = [];
+const myLibrary = [
+    { author: "Derrick Landy", title: "Skullduggery Pleasant", Pages: 300, read: false}
+];
 
 // Constructor for book object
 function book(author, title, pages, read){
