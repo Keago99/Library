ef270265869508e1268ad1bc5195aa87ba9f6e6d
Keago99 <keatabde@gmail.com>
2023-09-09 20:22:38 +0200

    Complete preventDefault(); working on adding and displaying new books in library, including updating the table with Arraylist.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // I'll be overcommenting everything for learning purposes, apologies if this seems annoying
 
 // vairable for the form
-const form = document.getElementById("addBook");
+const signupForm = document.getElementById("addBook");
 
 // All the elements from the input fields converted to
 // const to create new objects.
@@ -9,6 +9,7 @@ const author = document.getElementById("bAuthor");
 const title = document.getElementById("bTitle");
 const pages = document.getElementById("pages");
 const read = document.getElementById("read");
+
 
 // This is where all library files are stored
 const myLibrary = [
@@ -24,8 +25,24 @@ function book(author, title, pages, read){
     this.read = read
 }
 
-function addBookToLibrary(author, title, pages, read){
+// A function to add a book to the library.
+function addBookToLibrary(){
     const newBook = new book(author, title, pages, read)
     myLibrary.push(newBook);
-
 }
+
+function updateTable()
+{
+    myLibrary.forEach((book) => {
+        
+    });
+}
+
+signupForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    addBookToLibrary();
+    console.log(myLibrary[0]);
+});
+
+
+
