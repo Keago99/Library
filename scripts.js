5402e72c457bf5e81f512781d2831e15ed98ea01
Keago99 <keatabde@gmail.com>
2023-09-12 15:07:38 +0200

    Add a bit of style and a header, will continue with modals and editing.

--- scripts.js
+++ scripts.js
@@ -5,10 +5,10 @@ const signupForm = document.getElementById("addBook");
 
 // All the elements from the input fields converted to
 // const to create new objects.
-const author = document.getElementById("bAuthor");
-const title = document.getElementById("bTitle");
-const pages = document.getElementById("pages");
-const read = document.getElementById("read");
+let author = document.getElementById("bAuthor").value;
+let title = document.getElementById("bTitle").value;
+let pages = document.getElementById("pages").value;
+let read = document.getElementById("read");
 
 
 // This is where all library files are stored
@@ -19,15 +19,16 @@ const myLibrary = [
 // Constructor for book object
 function book(author, title, pages, read){
 
-    this.author = author
-    this.title = title
-    this.pages = pages
-    this.read = read
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
 }
 
 // A function to add a book to the library.
 function addBookToLibrary(){
-    const newBook = new book(author, title, pages, read)
+    checkBookInputs();
+    const newBook = new book(author, title, pages, read);
     myLibrary.push(newBook);
 }
 
@@ -41,8 +42,17 @@ function updateTable()
 signupForm.addEventListener("submit", function(event){
     event.preventDefault();
     addBookToLibrary();
-    console.log(myLibrary[0]);
+    myLibrary.forEach(function(element) {
+        console.log(element);
+    });
 });
+
+function checkBookInputs(){
+    author = document.getElementById("bAuthor").value;
+    title = document.getElementById("bTitle").value;
+    pages = document.getElementById("pages").value;
+}
+
 
 
 
